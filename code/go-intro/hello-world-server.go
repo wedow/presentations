360fc8edcf58a24ab8df4b38cad22b0288ddfb0f
Greg Wedow <gwedow@loyalty.com>
2018-02-23 18:23:08 +0000

    add stdout logging to example server

--- code/go-intro/hello-world-server.go
+++ code/go-intro/hello-world-server.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 var calls = 0
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	calls++
+	fmt.Fprintf(os.Stdout, "Request for: %s\n", r.URL.Path)
 	fmt.Fprintf(w, "Hello!\nCall number %d\n", calls)
 }
 
